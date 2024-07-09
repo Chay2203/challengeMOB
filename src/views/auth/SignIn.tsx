66ede8ca188be97fdc5db5128eef93ddbd71b418
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-10 00:09:05 +0530

    signin link

--- src/views/auth/SignIn.tsx
+++ src/views/auth/SignIn.tsx
@@ -39,7 +39,7 @@ export default function SignIn() {
     email: string,
     password: string
   ): Promise<Boolean> => {
-    const response = await fetch("http://localhost:3000/user", {
+    const response = await fetch("https://challangemob.onrender.com/user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
