ea736d4d257bc88d30b67ad1b5031d16028c43d0
Manish Rachakonda <108692670+manish-gitx@users.noreply.github.com>
2024-07-11 13:59:16 +0530

    adding the doubts card (#4)
    Signed-off-by: Manish Rachakonda <108692670+manish-gitx@users.noreply.github.com>

--- src/views/course/CoursePage.tsx
+++ src/views/course/CoursePage.tsx
@@ -3,7 +3,20 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Sidebar from 'components/sidebar';
 import CourseCard from 'components/card/CourseCard';
-import nft from '../../assets/img/nfts/Nft2.png';
+interface Course {
+  id?: number;
+  title: string;
+  new_field: string;
+  img_src: string;
+  week: number;
+
+  assests: {
+    yt_src: string;
+    res_src: string;
+    doc_src: string;
+    do_src:string;
+  };
+}
 
 const CoursePage = () => {
   const [open, setOpen] = useState(false);
@@ -11,18 +24,18 @@ const CoursePage = () => {
   const [showVideo, setShowVideo] = useState(false);
   const [videoUrl, setVideoUrl] = useState('');
   const videoRef = useRef(null);
-
+  const[limit,setlimt]=useState(0);
   useEffect(() => {
     const fetchCourses = async () => {
       try {
         const response = await axios.get('https://challangemob.onrender.com/courses');
-        const days = response.data.courses.sort((a: any, b: any) => a.week - b.week);
-        setCourses(days); 
+        const days: any = response.data.courses.sort((a: any, b: any) => a.week - b.week);
+        setCourses(days);
+        setlimt(days[0].limit);
       } catch (error) {
         console.error("Error fetching courses:", error);
       }
     };
-
     fetchCourses();
   }, []);
 
@@ -41,22 +54,36 @@ const CoursePage = () => {
   return (
     <div>
       <Sidebar open={open} onClose={() => setOpen(false)} />
-      <div className="ml-5 mt-5">
+      <div className="mt-5 ml-5">
         <div className="mb-5">
           <Link to="/admin" className="w-max lg:pt-5">
-            <div className="items-left flex h-fit hover:cursor-pointer"></div>
+            <div className="flex h-fit items-left hover:cursor-pointer">
+              <svg
+                width="8"
+                height="12"
+                viewBox="0 0 8 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6.70994 2.11997L2.82994 5.99997L6.70994 9.87997C7.09994 10.27 7.09994 10.9 6.70994 11.29C6.31994 11.68 5.68994 11.68 5.29994 11.29L0.709941 6.69997C0.319941 6.30997 0.319941 5.67997 0.709941 5.28997L5.29994 0.699971C5.68994 0.309971 6.31994 0.309971 6.70994 0.699971C7.08994 1.08997 7.09994 1.72997 6.70994 2.11997V2.11997Z"
+                  fill="#A3AED0"
+                />
+              </svg>
+              <p className="ml-3 text-sm text-gray-600">Back to Dashboard</p>
+            </div>
           </Link>
         </div>
         {showVideo && (
-          <div
+          <div 
             style={{
-              position: "fixed",
+              position: 'fixed',
               top: 0,
               left: 0,
-              width: "100%",
-              height: "100%",
+              width: '100%',
+              height: '100%',
               zIndex: 9999,
-              backgroundColor: "black",
+              backgroundColor: 'black',
             }}
           >
             <iframe
@@ -64,63 +91,49 @@ const CoursePage = () => {
               width="100%"
               height="100%"
               src={videoUrl}
-              frameBorder="10"
+              frameBorder="0"
               allow="autoplay; fullscreen"
               allowFullScreen
             ></iframe>
-            <button
+            <button 
               onClick={() => setShowVideo(false)}
               style={{
-                position: "absolute",
-                top: "10px",
-                right: "10px",
+                position: 'absolute',
+                top: '10px',
+                right: '10px',
                 zIndex: 10000,
-                background: "white",
-                padding: "5px 10px",
-                borderRadius: "5px",
+                background: 'white',
+                padding: '5px 10px',
+                borderRadius: '5px',
               }}
             >
               Close
             </button>
           </div>
         )}
-        <Link to="/admin" className="w-max lg:pt-5">
-          <div className="items-left flex h-fit pb-5 hover:cursor-pointer">
-            <svg
-              width="8"
-              height="12"
-              viewBox="0 0 8 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6.70994 2.11997L2.82994 5.99997L6.70994 9.87997C7.09994 10.27 7.09994 10.9 6.70994 11.29C6.31994 11.68 5.68994 11.68 5.29994 11.29L0.709941 6.69997C0.319941 6.30997 0.319941 5.67997 0.709941 5.28997L5.29994 0.699971C5.68994 0.309971 6.31994 0.309971 6.70994 0.699971C7.08994 1.08997 7.09994 1.72997 6.70994 2.11997V2.11997Z"
-                fill="#A3AED0"
-              />
-            </svg>
-            <p className="ml-3 text-sm text-gray-600">Back to Dashboard</p>
-          </div>
-        </Link>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-          {courses.map((course, index) => (
-            <div
-              key={course.id || index}
-              className={
-                index !== 0 ? "pointer-events-none blur-sm filter" : ""
-              }
-            >
-              <CourseCard
-                title={course.title}
-                author={course.new_field}
-                image={nft}
-                yt_btn="https://www.youtube.com/watch?v=fAUpXxr-fz4"
-                onYouTubeClick={handleYouTubeClick}
-                res_btn="https://season-client-1ac.notion.site/Pre-Requisite-Resources-3cc74ef010594665a67f74fdeb680a14?pvs=4"
-                doc_btn="https://forms.gle/CA4aGcsnXm9k26xX6"
-                isActive={index === 0}
-              />
-            </div>
-          ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {courses.map((course, index) => {
+  
+
+  return (
+    <div
+      key={course.id || index}
+      className={index >= limit ? 'filter blur-sm pointer-events-none' : ''}
+    >
+      <CourseCard
+        title={course.title}
+        author={course.new_field}
+        image={course.img_src}  
+        yt_btn={course.yt_src} 
+        onYouTubeClick={handleYouTubeClick}
+        res_btn={course.res_src}
+        doc_btn={course.doc_src}
+        isActive={true}
+        do_btn={course.do_src}
+      />
+    </div>
+  );
+})}
         </div>
       </div>
     </div>
