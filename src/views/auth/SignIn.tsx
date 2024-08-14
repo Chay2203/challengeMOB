907136f595f21b8061861eb54cbeb6df3c49f9b4
Harshinireddy05 <140995666+Harshinireddy05@users.noreply.github.com>
2024-08-14 12:41:39 +0000

    corrected sign-up form alignment

--- src/views/auth/SignIn.tsx
+++ src/views/auth/SignIn.tsx
@@ -72,7 +72,7 @@ export default function SignIn() {
   return (
     <form
       onSubmit={handleLogin}
-      className="mb-16 mt-16 flex h-full w-full items-center justify-center px-2 md:mx-0 md:px-0 lg:mb-10 lg:items-center lg:justify-start"
+      className="mb-16 mt-40 flex h-full w-full items-center justify-center px-2 md:mx-0 md:px-0 lg:mb-10 lg:items-center lg:justify-start lg:translate-x-10"
     >
       {/* Sign in section */}
       <div className="mt-[10vh] w-full max-w-full flex-col items-center md:pl-4 lg:pl-0 xl:max-w-[420px]">
